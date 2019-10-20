286f691a33aca2ecc6368487daa136b2da1463b1
xiaoqiang <xiaoqplum@foxmail.com>
2019-10-21 02:43:51 +0800

    rabbitmq配置

--- rabbit-producer/src/main/java/com/xq/learn/rabbit/config/MessageConfig.java
+++ rabbit-producer/src/main/java/com/xq/learn/rabbit/config/MessageConfig.java
@@ -1,10 +1,6 @@
 package com.xq.learn.rabbit.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
